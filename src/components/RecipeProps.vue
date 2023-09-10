1e5f53f650b9bdcebe3d3844eb41a2d7e022cd0b
Islam Ahmed <softeng.islam@gmail.com>
2023-09-10 05:31:24 +0300

    Update RecipeProps.vue

--- src/components/RecipeProps.vue
+++ src/components/RecipeProps.vue
@@ -4,13 +4,13 @@
 	}
 </script>
 <template lang="pug">
-ul(class="overflow-hidden w-full mb-5 py-2 px-4 flex flex-row items-start justify-between bg-[var(--dark100)]")
-	li(class="flex flex-col")
+ul(class="overflow-hidden w-full mb-5 py-2 px-4 flex flex-row gap-2 items-start justify-between bg-[var(--dark100)]")
+	li(class="flex flex-col items-center justify-center w-1/4 h-full bg-[var(--dark300)] rounded-lg p-2")
 		span(class="text-white") Type of #[span(class="text-[var(--favColor)]") Lye ]:
 		select(class="text-sm text-green-500 h-6 bg-[var(--dark400)] outline-none" id="Units")
 			option(value="Gram") NaOH
 			option(value="Kilo") KOH
-	li(class="flex flex-col")
+	li(class="flex flex-col items-center justify-center w-1/4 h-full bg-[var(--dark300)] rounded-lg p-2")
 		span(class="text-white") Weight of #[span(class="text-[var(--favColor)]") Oils ]:
 		div(class="flex flex-row")
 			input(type="number" class="w-1/2 bg-[var(--dark200)] placeholder:text-white text-white pl-2 py-1 rounded-md scale-90" value="100")
@@ -18,7 +18,7 @@ ul(class="overflow-hidden w-full mb-5 py-2 px-4 flex flex-row items-start justif
 				option(value="Pounds") Pounds
 				option(value="Ounces") Ounces
 				option(value="Grams") Grams
-	li(class="flex flex-col")
+	li(class="flex flex-col items-center justify-center w-1/4 h-full bg-[var(--dark300)] rounded-lg p-2")
 		span(class="text-[var(--favColor)]") Water #[span(class="text-white") :]
 		div(class="flex flex-row")
 			select(class="bg-[var(--dark200)] text-blue-500 pl-2 py-1 rounded-md scale-90")
@@ -26,7 +26,7 @@ ul(class="overflow-hidden w-full mb-5 py-2 px-4 flex flex-row items-start justif
 				option(value="Lye Concentration") Lye Concentration
 				option(value="Water : Lye Ratio") Water : Lye Ratio
 			input(class="w-1/2 bg-[var(--dark200)] placeholder:text-white text-white pl-2 py-1 rounded-md scale-90" type="text" value="38")
-	li(class="flex flex-row")
+	li(class="flex flex-row items-center justify-center w-1/4 h-full bg-[var(--dark300)] rounded-lg p-2")
 		div(class="flex flex-col text-white")
 			span(class="") Super Fat
 			span(class="") Fragrance
