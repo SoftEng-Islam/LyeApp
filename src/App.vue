e4cb97f75c83b830dd815895b79852a02dccc0db
Islam Ahmed <softeng.islam@gmail.com>
2023-09-11 01:11:53 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -20,8 +20,8 @@ div(class="w-full h-full flex items-start dark:bg-[var(--dark400)]")
 		perfect-scrollbar(class="h-[90%] w-full flex items-center justify-center")
 			div(class="h-full w-full bg-[var(--dark400)] ")
 				div(class="flex flex-row gap-2 flex-wrap justify-center items-stretch [&_.lyeWidget]:w-72 [&_.lyeWidget]:p-2 [&_.lyeWidget]:rounded-lg [&_.lyeWidget]:bg-[var(--dark300)]")
-					<Info :OilProperties="OilsStore.selectedOilProprt" />
-					<AddedOils :OilSelected="OilsStore.selectedOilArray" />
+					<Info :OilProperties="OilsStore.selectedOilProprt"/>
+					<AddedOils :OilSelected="OilsStore.selectedOilArray"/>
 					RecipeTotal
 					SoapProperties
 	div(class="fixed bottom-5 right-5 flex items-center justify-center" style="-webkit-app-region: no-drag")
