b18f932726c598cc825280e7285fea078f0ff624
Islam Ahmed <softeng.islam@gmail.com>
2023-09-11 02:41:45 +0300

    update

--- src/components/RecipeProps.vue
+++ src/components/RecipeProps.vue
@@ -5,20 +5,22 @@
 </script>
 <template lang="pug">
 ul(class="overflow-hidden w-full mb-5 py-2 px-4 flex flex-row gap-2 items-start justify-between bg-[var(--dark100)]")
+	li(class="hover:border-[var(--favColor)] border border-transparent flex items-center justify-center w-1/4 h-full bg-[var(--dark300)] rounded-lg p-2")
+		span(class="text-white") Type of #[span(class="text-[var(--favColor)]") Lye]:
+		select(class="ml-2 text-sm text-green-500 h-6 bg-[var(--dark400)] outline-none" id="Units")
+			option(value="NaOH")  NaOH
+			option(value="KOH")  KOH
 	li(class="hover:border-[var(--favColor)] border border-transparent flex flex-col items-center justify-center w-1/4 h-full bg-[var(--dark300)] rounded-lg p-2")
-		span(class="text-white") Type of #[span(class="text-[var(--favColor)]") Lye ]:
-		select(class="text-sm text-green-500 h-6 bg-[var(--dark400)] outline-none" id="Units")
-			option(value="Gram") NaOH
-			option(value="Kilo") KOH
-	li(class="hover:border-[var(--favColor)] border border-transparent flex flex-col items-center justify-center w-1/4 h-full bg-[var(--dark300)] rounded-lg p-2")
-		span(class="text-white") Weight of #[span(class="text-[var(--favColor)]") Oils ]:
+		span(class="text-white text-sm") Weight of #[span(class="text-[var(--favColor)]") Oils]:
 		div(class="flex flex-row items-center justify-center")
-			input(type="number" class="w-1/2 bg-[var(--dark200)] placeholder:text-white text-white pl-2 py-1 rounded-md scale-90" value="100")
+			input(type="number" class="w-1/4 bg-[var(--dark200)] placeholder:text-white text-white pl-2 py-1 rounded-md scale-90" value="100")
 			select(class="bg-[var(--dark200)] text-red-500 pl-2 py-1 rounded-md scale-90")
 				option(value="Pounds") Pounds
 				option(value="Ounces") Ounces
 				option(value="Grams") Grams
-	li(class="hover:border-[var(--favColor)] border border-transparent flex flex-col items-center justify-center w-1/4 h-full bg-[var(--dark300)] rounded-lg p-2")
+				option(value="Kilo") Kilo
+				option(value="Tons") Tons
+	li(class="hover:border-[var(--favColor)] border border-transparent text-sm flex flex-col items-center justify-center w-1/4 h-full bg-[var(--dark300)] rounded-lg p-2")
 		span(class="text-[var(--favColor)]") Water #[span(class="text-white") :]
 		div(class="flex flex-row items-center justify-center")
 			select(class="bg-[var(--dark200)] text-blue-500 pl-2 py-1 rounded-md scale-90")
@@ -26,17 +28,16 @@ ul(class="overflow-hidden w-full mb-5 py-2 px-4 flex flex-row gap-2 items-start 
 				option(value="Lye Concentration") Lye Concentration
 				option(value="Water : Lye Ratio") Water : Lye Ratio
 			input(class="w-1/2 bg-[var(--dark200)] placeholder:text-white text-white pl-2 py-1 rounded-md scale-90" type="text" value="38")
-	li(class="hover:border-[var(--favColor)] border border-transparent flex flex-row items-center justify-center w-1/4 h-full bg-[var(--dark300)] rounded-lg p-2")
-		div(class="flex flex-col text-white")
-			span(class="") Super Fat
-			span(class="") Fragrance
-			span(class="") Amount
-		div(class="flex flex-col")
-			input(class="w-1/2 bg-[var(--dark200)] placeholder:text-white text-white pl-2 py-1 rounded-md scale-90" value="5")
-			input(class="w-1/2 bg-[var(--dark200)] placeholder:text-white text-white pl-2 py-1 rounded-md scale-90")
-			input(class="w-1/2 bg-[var(--dark200)] placeholder:text-white text-white pl-2 py-1 rounded-md scale-90")
-		div(class="flex flex-col text-pink-500")
-			span(class="") %
-			span(class="") oz/lb
-
+	li(class="hover:border-[var(--favColor)] border border-transparent text-sm flex flex-col items-start justify-center w-1/4 h-full bg-[var(--dark300)] rounded-lg p-2")
+		div(class="flex justify-between items-center")
+			span(class="text-orange-400 mr-auto") Super Fat
+			input(class="pl-2 w-1/3 mx-auto bg-[var(--dark200)] placeholder:text-white text-white  py-1 rounded-md scale-90" type="number" value="5")
+			span(class="text-orange-400") %
+		div(class="flex justify-between items-center")
+			span(class="text-fuchsia-400 mr-auto") Fragrance
+			input(class="pl-2 w-1/3 mx-auto bg-[var(--dark200)] placeholder:text-white text-white  py-1 rounded-md scale-90" type="number" value="3")
+			span(class="text-fuchsia-400") oz/lb
+		div(class="flex justify-between items-center")
+			span(class="text-green-400 mr-auto") Amount
+			input(class="pl-2 w-1/3 mx-auto bg-[var(--dark200)] placeholder:text-white text-white  py-1 rounded-md scale-90" type="number" value="2")
 </template>
