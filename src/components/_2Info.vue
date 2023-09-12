c2df3c999e6207059e8d1a3460b62b4e4acf6795
Islam Ahmed <softeng.islam@gmail.com>
2023-09-12 06:58:31 +0300

    Update _2Info.vue

--- src/components/_2Info.vue
+++ src/components/_2Info.vue
@@ -14,5 +14,4 @@ div(class="lyeWidget flex flex-col min-h-[200px] ")
 		span(class="text-black font-bold ") {{ OilProperties.name || 'Oil' }} Properties.
 	ul(class="bg-[var(--dark400)] mt-4 p-2 rounded-md h-full")
 		li(v-for="val , key in OilProperties") #[span(class="float-left text-white") {{key.charAt(0).toUpperCase() + key.slice(1)}} ] #[span(class="text-yellow-500") :] #[span(class="float-right text-yellow-500") {{ val }}]
-
 </template>
