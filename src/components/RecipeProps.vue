a347eaa92478ccfe9ac5cc88bc38a21e00873c8f
Islam Ahmed <softeng.islam@gmail.com>
2023-10-03 08:39:55 +0300

    Update RecipeProps.vue

--- src/components/RecipeProps.vue
+++ src/components/RecipeProps.vue
@@ -1,4 +1,5 @@
 <script lang="ts">
+// Your Code
 </script>
 <template lang="pug">
 ul(class="overflow-hidden w-full mb-5 py-2 px-4 flex flex-row gap-2 items-start justify-between bg-[var(--dark100)]")
