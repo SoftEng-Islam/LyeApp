aab63ed4a6ae080ddad485d41a4623bdcce44431
Islam Ahmed <softeng.islam@gmail.com>
2023-10-05 01:54:20 +0300

    Update _5SoapProperties.vue

--- src/components/_5SoapProperties.vue
+++ src/components/_5SoapProperties.vue
@@ -7,6 +7,53 @@ export default {
 div(class="lyeWidget flex flex-col min-h-[200px] border border-transparent hover:border-pink-500")
 	div(class="Head bg-pink-500 p-2 rounded-md")
 		span(class="text-black font-bold ") Soap Properties.
-	ul(class="bg-[var(--dark400)] mt-4 p-2 rounded-md h-full")
-		li()
+	perfect-scrollbar
+		ul(class="bg-[var(--dark400)] mt-4 p-2 rounded-md h-full")
+			li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
+				span(class="text-sm text-white") Hardness
+				span(class="text-blue-500 ml-auto") 100 #[span(class="text-gray-500") GRAM]
+			li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
+				span(class="text-sm text-white") Cleansing
+				span(class="text-blue-500 ml-auto") 100 #[span(class="text-gray-500") GRAM]
+			li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
+				span(class="text-sm text-white") Conditioning
+				span(class="text-blue-500 ml-auto") 100 #[span(class="text-gray-500") GRAM]
+			li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
+				span(class="text-sm text-white") Bubbly
+				span(class="text-blue-500 ml-auto") 100 #[span(class="text-gray-500") GRAM]
+			li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
+				span(class="text-sm text-white") Creamy
+				span(class="text-blue-500 ml-auto") 100 #[span(class="text-gray-500") GRAM]
+			li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
+				span(class="text-sm text-white") Iodine
+				span(class="text-blue-500 ml-auto") 100 #[span(class="text-gray-500") GRAM]
+			li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
+				span(class="text-sm text-white") INS
+				span(class="text-blue-500 ml-auto") 100 #[span(class="text-gray-500") GRAM]
+			li()
+				hr(class="opacity-50")
+			li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
+				span(class="text-sm text-white") Lauric
+				span(class="text-blue-500 ml-auto") 100 #[span(class="text-gray-500") GRAM]
+			li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
+				span(class="text-sm text-white") Myristic
+				span(class="text-blue-500 ml-auto") 100 #[span(class="text-gray-500") GRAM]
+			li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
+				span(class="text-sm text-white") Palmitic
+				span(class="text-blue-500 ml-auto") 100 #[span(class="text-gray-500") GRAM]
+			li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
+				span(class="text-sm text-white") Stearic
+				span(class="text-blue-500 ml-auto") 100 #[span(class="text-gray-500") GRAM]
+			li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
+				span(class="text-sm text-white") Ricinoleic
+				span(class="text-blue-500 ml-auto") 100 #[span(class="text-gray-500") GRAM]
+			li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
+				span(class="text-sm text-white") Oleic
+				span(class="text-blue-500 ml-auto") 100 #[span(class="text-gray-500") GRAM]
+			li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
+				span(class="text-sm text-white") Linoleic
+				span(class="text-blue-500 ml-auto") 100 #[span(class="text-gray-500") GRAM]
+			li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
+				span(class="text-sm text-white") Linolenic
+				span(class="text-blue-500 ml-auto") 100 #[span(class="text-gray-500") GRAM]
 </template>
