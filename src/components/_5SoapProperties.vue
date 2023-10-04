991d51f3305334dd664a171bfea98a9dfb228586
Islam Ahmed <softeng.islam@gmail.com>
2023-10-04 22:23:34 +0300

    Update _5SoapProperties.vue

--- src/components/_5SoapProperties.vue
+++ src/components/_5SoapProperties.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
 </script>
 <template lang="pug">
-div(class="lyeWidget flex flex-col min-h-[200px] ")
+div(class="lyeWidget flex flex-col min-h-[200px] border border-transparent hover:border-blue-500")
 	div(class="Head bg-pink-500 p-2 rounded-md")
 		span(class="text-black font-bold ") Soap Properties.
 	ul(class="bg-[var(--dark400)] mt-4 p-2 rounded-md h-full")
