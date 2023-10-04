693c5de21a61453d56aa564e99369d21ebd2ba11
Islam Ahmed <softeng.islam@gmail.com>
2023-10-05 02:43:09 +0300

    Create PrintRecipe.vue

---
+++ src/components/PrintRecipe.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+export default {
+
+}
+</script>
+<template lang="pug">
+div(class="")
+div(class="")
+div(class="")
+div(class="")
+div(class="")
+div(class="")
+div(class="")
+div(class="")
+//- Write Your Note
+div(class="")
+</template>
