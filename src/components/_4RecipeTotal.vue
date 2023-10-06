42d308bc775702c88ce32d9a9b831c769eed4a34
Islam Ahmed <softeng.islam@gmail.com>
2023-10-07 00:06:29 +0300

    Update _4RecipeTotal.vue

--- src/components/_4RecipeTotal.vue
+++ src/components/_4RecipeTotal.vue
@@ -22,7 +22,7 @@ div(class="lyeWidget flex flex-col min-h-[200px] border border-transparent hover
 	ul(class="bg-[var(--dark400)] mt-4 p-2 rounded-md h-full")
 		li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
 			span(class="text-sm text-white") Water
-			span(class="text-blue-500 ml-auto duration-200") {{ weightWater || 100}} #[span(class="text-gray-500") GRAM]
+			span(class="text-blue-500 ml-auto duration-200") {{ weightWater || 0}} #[span(class="text-gray-500") GRAM]
 		li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
 			span(class="text-sm text-white") Lye - {{getTypeOfLye}}
 			span(class="text-blue-500 ml-auto duration-200") {{weightLye || 0}} #[span(class="text-gray-500") GRAM]
