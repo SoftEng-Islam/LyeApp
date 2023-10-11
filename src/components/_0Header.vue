6c568277ae3384579d2485b167f138534f7225c6
Islam Ahmed <softeng.islam@gmail.com>
2023-10-11 23:21:37 +0300

    Update _0Header.vue

--- src/components/_0Header.vue
+++ src/components/_0Header.vue
@@ -15,10 +15,10 @@ header(class="border-[var(--dark200)] border-b w-full flex items-center p-2 sele
 
 	//- Main App Fram
 	div(class="h-7 ml-auto mr-2 flex items-center justify-center" style="-webkit-app-region: no-drag")
-		button(class="duration-150 hover:opacity-50 w-8 h-8 rounded-lg bg-yellow-500" id="minimize")
+		button(class="duration-150 hover:opacity-50 w-7 h-7 rounded-lg bg-yellow-500" id="minimize")
 			<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="white"><path d="M12 16L6 10H18L12 16Z"/></svg>
-		button(class="duration-150 hover:opacity-50 w-8 h-8 mx-2 	rounded-lg bg-green-500" id="maximize")
+		button(class="duration-150 hover:opacity-50 w-7 h-7 mx-2 	rounded-lg bg-green-500" id="maximize")
 			<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="white"><path d="M12 8L18 14H6L12 8Z"/></svg>
-		button(class="duration-150 hover:opacity-50 w-8 h-8 p-1 	rounded-lg bg-red-500" id="closeBtn")
+		button(class="duration-150 hover:opacity-50 w-7 h-7 p-1 	rounded-lg bg-red-500" id="closeBtn")
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" fill="white" stroke="white" stroke-width="2"><path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/></svg>
 </template>
