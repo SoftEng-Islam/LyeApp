2b77b00aadb2865d55b3ae8cb941433300365c98
Islam Ahmed <softeng.islam@gmail.com>
2023-10-15 23:36:13 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import Header from "./components/HeaderApp.vue";
-import Oils from "./components/OilsList.vue";
+import HeaderApp from "./components/HeaderApp.vue";
+import OilsList from "./components/OilsList.vue";
 import InfoOils from "./components/OilInfo.vue";
 import AddedOils from "./components/AddedOils.vue";
 import RecipeTotal from "./components/RecipeTotal.vue";
@@ -10,11 +10,11 @@ import RecipeProps from "./components/RecipeProps.vue";
 </script>
 
 <template lang="pug">
-Header
+header-app
 div(class="w-full h-full flex items-start dark:bg-[var(--dark400)]")
-	Oils
+	oils-list
 	div(class="h-full w-full flex flex-col items-center justify-start")
-		RecipeProps
+		recipe-props
 		perfect-scrollbar(class="h-[90%] w-full flex items-center justify-center")
 			div(class="h-full w-full bg-[var(--dark400)] ")
 				div(class="flex flex-row gap-4 flex-wrap justify-center items-stretch [&_.lyeWidget]:w-72 [&_.lyeWidget]:p-2 [&_.lyeWidget]:rounded-lg [&_.lyeWidget]:bg-[var(--dark300)]")
