60fa4776c34c444271ccde2356169985fa97ac93
Islam Ahmed <softeng.islam@gmail.com>
2023-10-16 23:07:15 +0300

    Update HeaderApp.vue

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -9,7 +9,7 @@ export default {
 };
 </script>
 <template lang="pug">
-header(class="border-[var(--dark200)] border-b w-full flex items-center justify-between p-2 select-none h-[var(--HeaderHeight)] bg-[var(--dark300)]" style="-webkit-app-region: drag;")
+header(class="w-full p-2 flex items-center justify-between select-none h-[var(--HeaderHeight)] bg-[var(--dark300)] border-b border-[var(--dark200)]" style="-webkit-app-region: drag;")
 	//- Logo
 	a(class="ml-4 text-xl outline-none text-[var(--favColor)]" :href="LogoLink" style="-webkit-app-region: no-drag") LyeApp #[span(class="text-xs opacity-70") v1.0]
 
@@ -24,7 +24,7 @@ header(class="border-[var(--dark200)] border-b w-full flex items-center justify-
 			li(data-color="var(--pink)"   class="duration-200 hover:scale-90 cursor-pointer h-5 w-5 rounded-lg m-1 bg-[var(--pink)]")
 
 	//- Main App Fram
-	div(class="h-7 ml-auto mr-2 flex items-center justify-center" style="-webkit-app-region: no-drag")
+	div(class="h-7 mr-2 flex items-center justify-center" style="-webkit-app-region: no-drag")
 		button(class="duration-150 hover:opacity-50 w-7 h-7 rounded-lg bg-yellow-500" id="minimize")
 			<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="white"><path d="M12 16L6 10H18L12 16Z"/></svg>
 		button(class="duration-150 hover:opacity-50 w-7 h-7 mx-2 	rounded-lg bg-green-500" id="maximize")
