7dfef946a9d3a3450b408a8a0556be522ccf4032
Islam Ahmed <softeng.islam@gmail.com>
2023-10-25 21:34:10 +0300

    Update AddedOils.vue

--- src/components/AddedOils.vue
+++ src/components/AddedOils.vue
@@ -31,6 +31,7 @@ div(class="lyeWidget flex flex-col min-h-[200px] border border-transparent hover
 	//- List of added oils
 	ul(class="bg-[var(--dark400)] mt-4 p-2 rounded-md h-full overflow-hidden")
 		added-oil(v-for="oil of AddedOils" :oil="oil")
+	//- weight of all oils
 	div(class="flex items-center overflow-hidden px-2 h-10")
 		span(class="text-sm text-white") Oils
 		span(class="text-green-500 ml-auto bg-[var(--dark400)] rounded-full px-2 border border-[var(--dark200)]") {{ OilStore.RecipeTotal.value.weightOils }} #[span(class="text-gray-300") Gram]
