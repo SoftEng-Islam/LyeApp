a16ff70f0146bdfe8ee2f11521c570f078aac9db
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2023-10-29 20:33:30 +0200

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -79,6 +79,9 @@ export const useOilStore = defineStore("taskStore", {
 		getTypeOfLye(): string {
 			return this.headerOptions.typeOfLye;
 		},
+		getRecipeTotal(): any {
+			return this.RecipeTotal;
+		},
 		getSoapProperties(): any {
 			return this.soapProperties;
 		}
