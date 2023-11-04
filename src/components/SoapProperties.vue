42f72b7c9845f81c62ddeb359d91539fc4f85720
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2023-11-04 18:59:37 +0200

    Update SoapProperties.vue

--- src/components/SoapProperties.vue
+++ src/components/SoapProperties.vue
@@ -56,7 +56,7 @@ div(class="lyeWidget flex flex-col min-h-[200px] max-h-[600px] border border-tra
 				span(class="text-pink-500 ml-auto") {{soapProperties.Cleansing}}
 			li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
 				span(class="text-sm text-white flex items-center")
-					<button class="w-4 h-4 rounded-full flex items-center justify-center bg-pink-500 text-sm mr-2 text-black" v-tippy="{ content: aryRanges[0] }">!</button>
+					<button class="w-4 h-4 rounded-full flex items-center justify-center bg-pink-500 text-sm mr-2 text-black" v-tippy="{ content: aryRanges[2] }">!</button>
 					| Conditioning
 				span(class="text-pink-500 ml-auto") {{soapProperties.Conditioning}}
 			li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-[var(--dark200)]")
