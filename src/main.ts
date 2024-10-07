9be74bba32bc6c603abb019876c0b305b3d71ae4
Islam Ahmed <softeng.islam@gmail.com>
2024-10-07 22:08:30 +0300

    we working...

--- src/main.ts
+++ src/main.ts
@@ -8,20 +8,19 @@ import "animate.css";
 // import PerfectScrollbar from "vue3-perfect-scrollbar";
 // import "vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css";
 
-
-import VueTippy from 'vue-tippy';
-import 'tippy.js/dist/tippy.css'; // optional for styling
+import VueTippy from "vue-tippy";
+import "tippy.js/dist/tippy.css"; // optional for styling
 
 createApp(App)
 	.use(createPinia())
 	.use(VueTippy, {
-		directive: 'tippy', // => v-tippy
-		component: 'tippy', // => <tippy/>
-		componentSingleton: 'tippy-singleton', // => <tippy-singleton/>,
+		directive: "tippy", // => v-tippy
+		component: "tippy", // => <tippy/>
+		componentSingleton: "tippy-singleton", // => <tippy-singleton/>,
 		defaultProps: {
-			placement: 'auto-end',
-			allowHTML: true,
-		}, // => Global default options * see all props
+			placement: "auto-end",
+			allowHTML: true
+		} // => Global default options * see all props
 	})
 	// .use(PerfectScrollbar, {
 	// 	watchOptions: true,
--- src/store/index.ts
+++ src/store/index.ts
@@ -11,13 +11,13 @@ export const useOilStore = defineStore("taskStore", {
 				selcted: 0,
 				waterAsOfOils: 38,
 				lyeConcentration: 70,
-				WaterToLyeRatio: "2:1",
+				WaterToLyeRatio: "2:1"
 			},
 			superFat: 0,
 			fragrance: {
 				value: 0,
-				frWeight: "g/Kg",
-			},
+				frWeight: "g/Kg"
+			}
 		},
 		OilProperties: {
 			Name: "Oil Properties",
@@ -35,13 +35,13 @@ export const useOilStore = defineStore("taskStore", {
 			Ricinoleic: 0,
 			Oleic: 0,
 			Linoleic: 0,
-			Linolenic: 0,
+			Linolenic: 0
 		},
 		RecipeTotal: {
 			weightLye: 0,
 			weightWater: 0,
 			weightOils: 0,
-			FragranceWeight: 0,
+			FragranceWeight: 0
 		},
 		AddedOils: new Array(),
 		soapProperties: {
@@ -67,9 +67,9 @@ export const useOilStore = defineStore("taskStore", {
 	}),
 	getters: {
 		GetOil(): Object {
-			return this.Oils.filter((O: { Name: any; }) => O.Name);
+			return this.Oils.filter((O: { Name: any }) => O.Name);
 		},
-		GetHeaderOptions(value: any): any{
+		GetHeaderOptions(value: any): any {
 			return this.headerOptions[value];
 		},
 		GetSelectedOils(): Object {
@@ -92,15 +92,13 @@ export const useOilStore = defineStore("taskStore", {
 				this.RecipeTotal.weightLye = 0;
 				// this.weightWater = 0;
 
-				this.AddedOils.forEach((oi: { weight: number; NaOH: number; KOH: number; }) => {
+				this.AddedOils.forEach((oi: { weight: number; NaOH: number; KOH: number }) => {
 					if (this.headerOptions.typeOfLye === "NaOH") {
 						let NaOH: number = oi.weight * oi.NaOH;
 
 						this.RecipeTotal.weightLye += parseInt(NaOH.toFixed(0));
 					} else {
-						this.RecipeTotal.weightLye += parseInt(
-							(oi.weight * oi.KOH).toFixed(0)
-						);
+						this.RecipeTotal.weightLye += parseInt((oi.weight * oi.KOH).toFixed(0));
 					}
 				});
 			}
@@ -118,33 +116,30 @@ export const useOilStore = defineStore("taskStore", {
 			this.OilProperties = selectedOil;
 		},
 		WaterAsofOils(valueOne: any, valueTwo: any): void {
-			if(valueOne == 0) {
+			if (valueOne == 0) {
 				this.headerOptions.water.waterAsOfOils = valueTwo;
 			} else if (valueOne == 1) {
 				this.headerOptions.water.lyeConcentration = valueTwo;
 			} else {
 				this.headerOptions.water.WaterToLyeRatio = valueTwo;
 			}
-			console.log('weightWater' , this.RecipeTotal.weightWater);
-			console.log('waterAsOfOils' , this.headerOptions.water.waterAsOfOils);
-			console.log('weightOils' , this.RecipeTotal.weightOils);
-			console.log('weightWater' , this.RecipeTotal.weightWater);
+			console.log("weightWater", this.RecipeTotal.weightWater);
+			console.log("waterAsOfOils", this.headerOptions.water.waterAsOfOils);
+			console.log("weightOils", this.RecipeTotal.weightOils);
+			console.log("weightWater", this.RecipeTotal.weightWater);
 
 			this.RecipeTotal.weightWater = (this.RecipeTotal.weightOils / 100) * this.headerOptions.water.waterAsOfOils;
 		},
-		calcLye(){
+		calcLye() {
 			this.RecipeTotal.weightOils = 0;
 			this.RecipeTotal.weightLye = 0;
 			// this.RecipeTotal.weightWater = 0;
-			this.AddedOils.forEach((oi: { weight: number; NaOH: number; KOH: number; }) => {
+			this.AddedOils.forEach((oi: { weight: number; NaOH: number; KOH: number }) => {
 				if (this.headerOptions.typeOfLye === "NaOH") {
-					let NaOH =
-						oi.weight * oi.NaOH;
+					let NaOH = oi.weight * oi.NaOH;
 					this.RecipeTotal.weightLye += parseInt(NaOH.toFixed(0));
 				} else {
-					this.RecipeTotal.weightLye += parseInt(
-						(oi.weight * oi.KOH).toFixed(0)
-					);
+					this.RecipeTotal.weightLye += parseInt((oi.weight * oi.KOH).toFixed(0));
 				}
 				this.RecipeTotal.weightOils += parseInt(oi.weight.toFixed(0));
 			});
@@ -178,27 +173,78 @@ export const useOilStore = defineStore("taskStore", {
 			this.soapProperties.Saturated = 0;
 			this.soapProperties.MonoUnsaturated = 0;
 			this.soapProperties.PolyUnsaturated = 0;
-			this.AddedOils.forEach((ele: { Hardness: number; weight: number; Cleansing: number; Condition: number; Bubbly: number; Creamy: number; Iodine: number; INS: number; Lauric: number; Myristic: number; Palmitic: number; Stearic: number; Ricinoleic: number; Oleic: number; Linoleic: number; Linolenic: number; Saturated: number; }) => {
-				this.soapProperties.Hardness += Math.round(ele.Hardness * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-				this.soapProperties.Cleansing += Math.round(ele.Cleansing * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-				this.soapProperties.Conditioning += Math.round(ele.Condition * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-				this.soapProperties.Bubbly += Math.round(ele.Bubbly * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-				this.soapProperties.Creamy += Math.round(ele.Creamy * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-				this.soapProperties.Iodine += Math.round(ele.Iodine * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-				this.soapProperties.INS += Math.round(ele.INS * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-				this.soapProperties.Lauric += Math.round(ele.Lauric * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-				this.soapProperties.Myristic += Math.round(ele.Myristic * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-				this.soapProperties.Palmitic += Math.round(ele.Palmitic * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-				this.soapProperties.Stearic += Math.round(ele.Stearic * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-				this.soapProperties.Ricinoleic += Math.round(ele.Ricinoleic * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-				this.soapProperties.Oleic += Math.round(ele.Oleic * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-				this.soapProperties.Linoleic += Math.round(ele.Linoleic * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-				this.soapProperties.Linolenic += Math.round(ele.Linolenic * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-				this.soapProperties.Saturated += Math.round(ele.Saturated * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-				// this.soapProperties.MonoUnsaturated += Math.round(ele.Id * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-				// this.soapProperties.PolyUnsaturated += Math.round(ele.Id * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
-			}
-			)
+			this.AddedOils.forEach(
+				(ele: {
+					Hardness: number;
+					weight: number;
+					Cleansing: number;
+					Condition: number;
+					Bubbly: number;
+					Creamy: number;
+					Iodine: number;
+					INS: number;
+					Lauric: number;
+					Myristic: number;
+					Palmitic: number;
+					Stearic: number;
+					Ricinoleic: number;
+					Oleic: number;
+					Linoleic: number;
+					Linolenic: number;
+					Saturated: number;
+				}) => {
+					this.soapProperties.Hardness += Math.round(
+						(ele.Hardness * ((ele.weight * 100) / this.RecipeTotal.weightOils)) / 100
+					);
+					this.soapProperties.Cleansing += Math.round(
+						(ele.Cleansing * ((ele.weight * 100) / this.RecipeTotal.weightOils)) / 100
+					);
+					this.soapProperties.Conditioning += Math.round(
+						(ele.Condition * ((ele.weight * 100) / this.RecipeTotal.weightOils)) / 100
+					);
+					this.soapProperties.Bubbly += Math.round(
+						(ele.Bubbly * ((ele.weight * 100) / this.RecipeTotal.weightOils)) / 100
+					);
+					this.soapProperties.Creamy += Math.round(
+						(ele.Creamy * ((ele.weight * 100) / this.RecipeTotal.weightOils)) / 100
+					);
+					this.soapProperties.Iodine += Math.round(
+						(ele.Iodine * ((ele.weight * 100) / this.RecipeTotal.weightOils)) / 100
+					);
+					this.soapProperties.INS += Math.round(
+						(ele.INS * ((ele.weight * 100) / this.RecipeTotal.weightOils)) / 100
+					);
+					this.soapProperties.Lauric += Math.round(
+						(ele.Lauric * ((ele.weight * 100) / this.RecipeTotal.weightOils)) / 100
+					);
+					this.soapProperties.Myristic += Math.round(
+						(ele.Myristic * ((ele.weight * 100) / this.RecipeTotal.weightOils)) / 100
+					);
+					this.soapProperties.Palmitic += Math.round(
+						(ele.Palmitic * ((ele.weight * 100) / this.RecipeTotal.weightOils)) / 100
+					);
+					this.soapProperties.Stearic += Math.round(
+						(ele.Stearic * ((ele.weight * 100) / this.RecipeTotal.weightOils)) / 100
+					);
+					this.soapProperties.Ricinoleic += Math.round(
+						(ele.Ricinoleic * ((ele.weight * 100) / this.RecipeTotal.weightOils)) / 100
+					);
+					this.soapProperties.Oleic += Math.round(
+						(ele.Oleic * ((ele.weight * 100) / this.RecipeTotal.weightOils)) / 100
+					);
+					this.soapProperties.Linoleic += Math.round(
+						(ele.Linoleic * ((ele.weight * 100) / this.RecipeTotal.weightOils)) / 100
+					);
+					this.soapProperties.Linolenic += Math.round(
+						(ele.Linolenic * ((ele.weight * 100) / this.RecipeTotal.weightOils)) / 100
+					);
+					this.soapProperties.Saturated += Math.round(
+						(ele.Saturated * ((ele.weight * 100) / this.RecipeTotal.weightOils)) / 100
+					);
+					// this.soapProperties.MonoUnsaturated += Math.round(ele.Id * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
+					// this.soapProperties.PolyUnsaturated += Math.round(ele.Id * (ele.weight * 100 / this.RecipeTotal.weightOils) /100);
+				}
+			);
 		},
 		AddedOilsWeight(OilName: string, OilWeight: number): void {
 			this.RecipeTotal.weightLye = 0;
@@ -211,31 +257,32 @@ export const useOilStore = defineStore("taskStore", {
 					oi.weight = OilWeight;
 				}
 				if (this.headerOptions.typeOfLye === "NaOH") {
-					let NaOH: number =
-						oi.weight * oi.NaOH;
+					let NaOH: number = oi.weight * oi.NaOH;
 
 					this.RecipeTotal.weightLye += parseInt(NaOH.toFixed(0));
 				} else {
-					this.RecipeTotal.weightLye += parseInt(
-						(oi.weight * oi.KOH).toFixed(0)
-					);
+					this.RecipeTotal.weightLye += parseInt((oi.weight * oi.KOH).toFixed(0));
 				}
 				this.RecipeTotal.weightOils += parseInt(oi.weight.toFixed(0));
 			});
 			// this.RecipeTotal.weightWater += parseInt((this.RecipeTotal.weightLye * 3).toFixed(0));
-			this.RecipeTotal.FragranceWeight = Math.round(((this.RecipeTotal.weightOils / 100) * this.headerOptions.fragrance.value) / 1000 * 100);
+			this.RecipeTotal.FragranceWeight = Math.round(
+				(((this.RecipeTotal.weightOils / 100) * this.headerOptions.fragrance.value) / 1000) * 100
+			);
 			this.WaterAsofOils();
 			this.getProperties();
 			this.ChangeSuperFat(this.headerOptions.superFat);
 		},
-		ChangeSuperFat(value:number): void {
+		ChangeSuperFat(value: number): void {
 			this.calcLye();
 			this.headerOptions.superFat = value;
 			this.RecipeTotal.weightLye = ((100 - this.headerOptions.superFat) / 100) * this.RecipeTotal.weightLye;
 		},
-		ChangeFragrance(value:number): void {
+		ChangeFragrance(value: number): void {
 			this.headerOptions.fragrance.value = value;
-			this.RecipeTotal.FragranceWeight = Math.round(((this.RecipeTotal.weightOils / 100) * this.headerOptions.fragrance.value) / 1000 * 100);
+			this.RecipeTotal.FragranceWeight = Math.round(
+				(((this.RecipeTotal.weightOils / 100) * this.headerOptions.fragrance.value) / 1000) * 100
+			);
 		}
-	},
+	}
 });
