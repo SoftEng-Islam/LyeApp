e5a9900da6af366b5de34ba2f56164d045c729e8
Islam Ahmed <softeng.islam@gmail.com>
2024-10-11 04:33:59 +0300

    we working...

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,13 @@ import RecipeOptions from "./components/RecipeOptions.vue";
 
 <template lang="pug">
 <AppHeader/>
-div(class="w-full h-full flex items-start dark:bg-[var(--dark400)]")
-	<OilsList/>
-	div(class="flex flex-wrap")
-		<RecipeOptions/>
-		PerfectScrollbar(class="h-[90%] w-full flex items-center justify-center")
-			div(class="h-full w-full bg-[var(--dark400)] ")
-				div(class="flex flex-row gap-4 flex-wrap justify-center items-stretch [&_.lyeWidget]:w-72 [&_.lyeWidget]:p-2 [&_.lyeWidget]:rounded-lg [&_.lyeWidget]:bg-[var(--dark100)]")
-					<OilProperties/>
-					<AddedOils/>
-					<TotalRecipe/>
-					<SoapProperties/>
+div(class="w-full h-full flex flex-wrap items-start dark:bg-[var(--dark400)]")
+	<RecipeOptions/>
+	PerfectScrollbar(class="h-[90%] w-full flex items-center justify-center")
+		div(class="h-full w-full flex flex-row gap-4 flex-wrap justify-center items-stretch [&_.lyeWidget]:w-72 [&_.lyeWidget]:p-2 [&_.lyeWidget]:rounded-lg [&_.lyeWidget]:bg-[var(--dark100)]")
+			<OilsList/>
+			<OilProperties/>
+			<AddedOils/>
+			<TotalRecipe/>
+			<SoapProperties/>
 </template>
